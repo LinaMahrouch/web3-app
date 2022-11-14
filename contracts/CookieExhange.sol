4f74d972372293446a78b99772a49008785251ec
ElinaaCodes <lina.mahrouch01@gmail.com>
2022-11-14 13:37:40 +0100

    Store message from users after connecting the wallet

--- contracts/CookieExhange.sol
+++ contracts/CookieExhange.sol
@@ -5,15 +5,29 @@ import "hardhat/console.sol";
 contract CookiePortal {
      uint256 totalCookies;
 
+event NewCookie(address indexed from, uint256 timestamp, string message);
+  struct Cookie {
+        address cookiegiver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+    Cookie[] cookies;
+
     constructor() {
         console.log("Hello I am a cookie!");
     }
-    
-    function cookie() public {
+   
+  function cookie(string memory _message) public {
         totalCookies += 1;
         console.log("%s cookie count:",msg.sender);
+        //store data in the array of cookies defined below
+        cookies.push(Cookie(msg.sender, _message, block.timestamp));
+        emit NewCookie(msg.sender, block.timestamp,_message);
     }
 
+ function getAllCookies() public view returns (Cookie[] memory) {
+        return cookies;
+    }
     function getTotalCookies() public view returns (uint256){
         console.log("We reached %d cookies Thank you!", totalCookies);
     }
--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,27 @@
 const main = async() =>
 {
-    const [owner,randomPerson]= await hre.ethers.getSigners();
+    //const [owner,randomPerson]= await hre.ethers.getSigners();
     //compile contract and generate the necessary files we need to work with our contract under the artifacts directory
     const cookieContractFactory = await hre.ethers.getContractFactory("CookiePortal");
     const cookieContract = await cookieContractFactory.deploy();
     await cookieContract.deployed();
-    console.log("contract is being deployed to:", cookieContract.address);
-    console.log("contract is being deployed by:", owner.address);
-    await cookieContract.getTotalCookies();
-    const firstcookieTkn = await cookieContract.cookie();
-    await firstcookieTkn.wait();
-    const secondcookieTkn = await cookieContract.connect(randomPerson).cookie();
-    await secondcookieTkn.wait();
+    // console.log("contract is being deployed to:", cookieContract.address);
+    // console.log("contract is being deployed by:", owner.address);
+    let cookieCount;
+    cookieCount= await cookieContract.getTotalCookies();
+    console.log(cookieCount.toNumber());
 
-    await cookieContract.getTotalCookies();
+    let  cookieTkn = await cookieContract.cookie("A simple message");
+    await cookieTkn.wait();//wait for mined transaction here
+    const [_, randomPerson] = await hre.ethers.getSigners(); 
+    cookieTkn = await cookieContract.connect(randomPerson).cookie("A simple message")
+    //const secondcookieTkn = await cookieContract.connect(randomPerson).cookie();
+    //await secondcookieTkn.wait();
+
+    let allCookies = await cookieContract.getAllCookies();
+    console.log(allCookies);
+
+    
 };
 
 const runMain = async () =>
